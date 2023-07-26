f67917c16013088dd3d0b8bf45ee4062a84439d8
CraciDenisStefan <crdenis13@gmail.com>
2023-07-26 16:51:48 +0200

    Esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,23 +15,46 @@
 
 </head>
 
-<body>
+<body class="bg-success-subtle">
     <div class="container">
         <div class="row">
-            @foreach($all_trains as $all_train)
-                
-                        {{ $all_train->azienda }}
-          
-            @endforeach 
-<br>
-            @foreach($trains_today as $train)
-               
-            {{ $train->azienda }}
-             
+            <h1>Treni in partenza da oggi :</h1>
+        @foreach($trains_today as $train)       
+            <div class="col-4 p-3">
+                <h3 class="text-success"><span class="fw-bolder">{{ $train->azienda }}</h3>
+                <div>Giorno partenza:<span class="fw-bolder"> {{ $train->data_partenza }}</span></div>
+                <div>Da: <span class="fw-bolder">{{ $train->stazione_partenza }}</span></div>
+                <div>A:<span> {{ $train->stazione_arrivo }}</span></div>
+                <div>Orario Partenza: <span class="fw-bolder">{{ $train->orario_partenza }}</span></div>
+                <div>Orario Arrivo:<span class="fw-bolder"> {{ $train->orario_arrivo }}</span></div>
+                <div>Codice:<span class="fw-bolder"> {{ $train->codice_treno }}</span></div>
+                <div>Carrozze: <span class="fw-bolder">{{ !empty($train->numero_carrozze) ? $train->numero_carrozze : 'N/A'}}</span></div>
+                <div>In Orario:<span class="fw-bolder"> {{ $train->in_orario ? 'Il treno è in orario!' : 'Il treno è in ritardo.' }}</span></div>
+                <div class="text-danger">{{ $train->cancellato? 'Cancellato' : ''}}</div>
+            </div>
         @endforeach 
         </div>
-      </div>
+      
 
+
+        <div class="row mt-5">
+            <h1>Tutti i treni :</h1>
+            @foreach($all_trains as $all_train)
+                <div class="col-4 p-3">
+                    <h3 class="text-success">{{ $all_train->azienda }}</h3>
+                    <div>Giorno partenza:<span class="fw-bolder"> {{ $all_train->data_partenza }}</span></div>
+                    <div>Da: <span class="fw-bolder">{{ $all_train->stazione_partenza }}</span></div>
+                    <div>A:<span class="fw-bolder"> {{ $all_train->stazione_arrivo }}</span></div>
+                    <div>Orario Partenza: <span class="fw-bolder">{{ $all_train->orario_partenza }}</span></div>
+                    <div>Orario Arrivo: <span class="fw-bolder">{{ $all_train->orario_arrivo }}</span></div>
+                    <div>Codice: <span class="fw-bolder">{{ $all_train->codice_treno }}</span></div>
+                    <div>Carrozze:<span class="fw-bolder"> {{ !empty($all_train->numero_carrozze) ? $all_train->numero_carrozze : 'N/A'}}</span></div>
+                    <div>In Orario:<span class="fw-bolder"> {{ $all_train->in_orario ? 'Il treno è in orario!' : 'Il treno è in ritardo.' }}</span></div>
+                    <div class="text-danger">{{ $all_train->cancellato? 'Cancellato' : ''}}</div>
+                </div>
+            @endforeach 
+        </div>
+    </div>
 </body>
 
 </html>
