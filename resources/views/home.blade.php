525b6091e69b08f9412df89622a04af51deced3f
CraciDenisStefan <crdenis13@gmail.com>
2023-07-26 17:03:07 +0200

    [FIX]Aggiunto forelse . Esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
     <div class="container">
         <div class="row">
             <h1>Treni in partenza da oggi :</h1>
-        @foreach($trains_today as $train)       
+        @forelse($trains_today as $train)       
             <div class="col-4 p-3">
                 <h3 class="text-success"><span class="fw-bolder">{{ $train->azienda }}</h3>
                 <div>Giorno partenza:<span class="fw-bolder"> {{ $train->data_partenza }}</span></div>
@@ -32,7 +32,11 @@
                 <div>In Orario:<span class="fw-bolder"> {{ $train->in_orario ? 'Il treno è in orario!' : 'Il treno è in ritardo.' }}</span></div>
                 <div class="text-danger">{{ $train->cancellato? 'Cancellato' : ''}}</div>
             </div>
-        @endforeach 
+        @empty
+        <div class="col-12">
+            <h3>Nessun treno in partenza oggi </h3>
+        </div>
+        @endforelse
         </div>
       
 
